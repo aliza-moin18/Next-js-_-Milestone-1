49df122ee149946a3c2ba8c827143217cc4c76f5
Aliza Moin <160245045+aliza-moin18@users.noreply.github.com>
2024-09-13 08:36:03 -0700

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+
+    <div style={{ display:'flex', justifyContent:'center', alignItems:'center', height: '100vh', color: '#ef0166', fontSize: '45px', fontWeight: 'bold', backgroundColor: '#181214'}}>
+
+      <h1>Hello World!</h1>
+
+    </div>
+    
+  );
+}
